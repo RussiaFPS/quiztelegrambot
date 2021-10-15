5dc60a37db9484368efb30416507fafce37f38c1
nikita_ne <nikitakrut58@gmail.com>
2021-10-15 23:58:06 +0300

    added start logic

--- bot.go
+++ bot.go
@@ -1,5 +1,57 @@
 package main
 
-func main(){
+import (
+	"bufio"
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+	"strings"
+)
 
+var bot,e=tgbot.NewBotAPI(getAPI())
+
+func getAPI() string {
+	var res string
+	file, err := os.Open("API.txt")
+	checkErr(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		res = scanner.Text()
+	}
+	checkErr(scanner.Err())
+	return strings.TrimSpace(res)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func main() {
+	checkErr(e)
+
+	u:=tgbot.NewUpdate(0)
+	u.Timeout= 60
+
+	updates:=bot.GetUpdatesChan(u)
+
+	for update:=range updates{
+		msg:=tgbot.NewMessage(update.Message.Chat.ID,"")
+
+		if !update.Message.IsCommand(){
+			continue
+		}
+		switch strings.ToLower(update.Message.Command()){
+		case "start":
+			msg.Text="Привет"
+		case "go":
+			msg.Text="Go"
+		default:
+			msg.Text="Я не знаю такую команду..."
+		}
+		_,err:=bot.Send(msg)
+		checkErr(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module quiztelegrambot
 
 go 1.17
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.2-0.20210820200834-309d612d7095
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
