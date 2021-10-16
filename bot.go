2c3017abfa29a3e9630267874729e1156b6a6468
nikita_ne <nikitakrut58@gmail.com>
2021-10-16 20:38:00 +0300

    create type for questions,added function which creates slice questions and answer

--- bot.go
+++ bot.go
@@ -4,11 +4,36 @@ import (
 	"bufio"
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 var bot,e=tgbot.NewBotAPI(getAPI())
+
+type questions struct {
+	quest string
+	answer1 string
+	answer2 string
+	answer3 string
+	want string
+}
+
+func createQuest() []questions {
+	slice:=[]questions{
+		{quest: "Кто из президентов США написал свой собственный рассказ про Шерлока Холмса?",answer1: "Джон Кеннеди",answer2: "Франклин Рузвельт",answer3: "Рональд Рейган",want:"Франклин Рузвельт"},
+		{quest: "Какую пошлину ввели в XII  веке в Англии для того чтобы заставить мужчин пойти на войну?",answer1: "Налог на тунеядство",answer2:"Налог на трусость",answer3: "Налог на отсутствие сапог",want: "Налог на трусость"},
+		{quest: "Откуда пошло выражение «деньги не пахнут?",answer1: "От подателей за провоз парфюмерии",answer2: "От сборов за нестиранные носки",answer3: "От налога на туалеты",want: "От налога на туалеты"},
+		{quest: "Туристы, приезжающие на Майорку, обязаны заплатить налог…",answer1: "На плавки",answer2: "На пальмы",answer3: "На солнце",want: "На солнце"},
+		{quest: "Один известный писатель рассказывал, что списал образ старушки-вредины со своей бывшей жены. При этом бабулька оказалась удивительно похожей на Коко Шанель. На голове у нее всегда была шляпка со складной тульей, благодаря которой она и получила прозвище",answer1: "Шапокляк",answer2: "Красная Шапочка",answer3: "Мадам Баттерфляй",want: "Шапокляк"},
+	}
+	for i := range slice {
+		j := rand.Intn(i + 1)
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+	return slice
+}
 
 func getAPI() string {
 	var res string
@@ -31,10 +56,11 @@ func checkErr(err error) {
 
 func main() {
 	checkErr(e)
+	rand.Seed(time.Now().Unix())
+	//quests:=createQuest()
 
 	u:=tgbot.NewUpdate(0)
 	u.Timeout= 60
-
 	updates:=bot.GetUpdatesChan(u)
 
 	for update:=range updates{
@@ -47,7 +73,7 @@ func main() {
 		case "start":
 			msg.Text="Привет"
 		case "go":
-			msg.Text="Go"
+			//go
 		default:
 			msg.Text="Я не знаю такую команду..."
 		}
