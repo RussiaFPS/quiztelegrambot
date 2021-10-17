37da9ab43b2000cc3514bc31c62ec0c6e5602ba1
nikita_ne <nikitakrut58@gmail.com>
2021-10-17 21:11:37 +0300

    added menu questions

--- bot.go
+++ bot.go
@@ -10,23 +10,23 @@ import (
 	"time"
 )
 
-var bot,e=tgbot.NewBotAPI(getAPI())
+var bot, e = tgbot.NewBotAPI(getAPI())
 
 type questions struct {
-	quest string
+	quest   string
 	answer1 string
 	answer2 string
 	answer3 string
-	want string
+	want    string
 }
 
 func createQuest() []questions {
-	slice:=[]questions{
-		{quest: "Кто из президентов США написал свой собственный рассказ про Шерлока Холмса?",answer1: "Джон Кеннеди",answer2: "Франклин Рузвельт",answer3: "Рональд Рейган",want:"Франклин Рузвельт"},
-		{quest: "Какую пошлину ввели в XII  веке в Англии для того чтобы заставить мужчин пойти на войну?",answer1: "Налог на тунеядство",answer2:"Налог на трусость",answer3: "Налог на отсутствие сапог",want: "Налог на трусость"},
-		{quest: "Откуда пошло выражение «деньги не пахнут?",answer1: "От подателей за провоз парфюмерии",answer2: "От сборов за нестиранные носки",answer3: "От налога на туалеты",want: "От налога на туалеты"},
-		{quest: "Туристы, приезжающие на Майорку, обязаны заплатить налог…",answer1: "На плавки",answer2: "На пальмы",answer3: "На солнце",want: "На солнце"},
-		{quest: "Один известный писатель рассказывал, что списал образ старушки-вредины со своей бывшей жены. При этом бабулька оказалась удивительно похожей на Коко Шанель. На голове у нее всегда была шляпка со складной тульей, благодаря которой она и получила прозвище",answer1: "Шапокляк",answer2: "Красная Шапочка",answer3: "Мадам Баттерфляй",want: "Шапокляк"},
+	slice := []questions{
+		{quest: "Кто из президентов США написал свой собственный рассказ про Шерлока Холмса?", answer1: "Джон Кеннеди", answer2: "Франклин Рузвельт", answer3: "Рональд Рейган", want: "Франклин Рузвельт"},
+		{quest: "Какую пошлину ввели в XII  веке в Англии для того чтобы заставить мужчин пойти на войну?", answer1: "Налог на тунеядство", answer2: "Налог на трусость", answer3: "Налог на отсутствие сапог", want: "Налог на трусость"},
+		{quest: "Откуда пошло выражение «деньги не пахнут?", answer1: "От подателей за провоз парфюмерии", answer2: "От сборов за нестиранные носки", answer3: "От налога на туалеты", want: "От налога на туалеты"},
+		{quest: "Туристы, приезжающие на Майорку, обязаны заплатить налог…", answer1: "На плавки", answer2: "На пальмы", answer3: "На солнце", want: "На солнце"},
+		{quest: "Один известный писатель рассказывал, что списал образ старушки-вредины со своей бывшей жены. При этом бабулька оказалась удивительно похожей на Коко Шанель. На голове у нее всегда была шляпка со складной тульей, благодаря которой она и получила прозвище", answer1: "Шапокляк", answer2: "Красная Шапочка", answer3: "Мадам Баттерфляй", want: "Шапокляк"},
 	}
 	for i := range slice {
 		j := rand.Intn(i + 1)
@@ -57,27 +57,43 @@ func checkErr(err error) {
 func main() {
 	checkErr(e)
 	rand.Seed(time.Now().Unix())
-	//quests:=createQuest()
+	quests := createQuest()
 
-	u:=tgbot.NewUpdate(0)
-	u.Timeout= 60
-	updates:=bot.GetUpdatesChan(u)
+	u := tgbot.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
 
-	for update:=range updates{
-		msg:=tgbot.NewMessage(update.Message.Chat.ID,"")
+	for update := range updates {
+		msg := tgbot.NewMessage(update.Message.Chat.ID, "")
 
-		if !update.Message.IsCommand(){
-			continue
+		if update.Message.IsCommand() {
+			switch strings.ToLower(update.Message.Command()) {
+			case "start":
+				msg.Text = "Привет"
+				_, err := bot.Send(msg)
+				checkErr(err)
+			case "go":
+				questObject := quests[rand.Intn(len(quests))]
+				var menu = tgbot.NewReplyKeyboard(
+					tgbot.NewKeyboardButtonRow(
+						tgbot.NewKeyboardButton(questObject.answer1),
+					),
+					tgbot.NewKeyboardButtonRow(
+						tgbot.NewKeyboardButton(questObject.answer2),
+					),
+					tgbot.NewKeyboardButtonRow(
+						tgbot.NewKeyboardButton(questObject.answer3),
+					),
+				)
+				msg.Text = questObject.quest
+				msg.ReplyMarkup = menu
+				_, err := bot.Send(msg)
+				checkErr(err)
+			default:
+				msg.Text = "Я не знаю такую команду..."
+				_, err := bot.Send(msg)
+				checkErr(err)
+			}
 		}
-		switch strings.ToLower(update.Message.Command()){
-		case "start":
-			msg.Text="Привет"
-		case "go":
-			//go
-		default:
-			msg.Text="Я не знаю такую команду..."
-		}
-		_,err:=bot.Send(msg)
-		checkErr(err)
 	}
 }
